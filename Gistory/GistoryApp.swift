82fc47e9b1246b8c0e3ac3ef106b1fb1dba35d36
hotcat0421 <s24044@gsm.hs.kr>
2024-12-16 22:47:31 +0900

    Initial Commit

---
+++ Gistory/GistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GistoryApp.swift
+//  Gistory
+//
+//  Created by 박정우 on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
